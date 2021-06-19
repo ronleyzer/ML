805e4726ee0b889dd09eb5bfe19319ed7fd7d3dc
Ron Leyzer <ron.l@SPARKS-AB.LOCAL>
2021-06-19 10:04:59 +0300

    data_structures

--- classes.py
+++ classes.py
@@ -29,68 +29,76 @@ class LinkedList(object):
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
-        i = 1
+        counter = 1
         current = self.head
-        if current:
-            while current.next:
-                if i < position:
-                    current = current.next
-                    i += 1
-                elif i == position:
-                    return current.value
-        else:
+        if position < 1:
             return None
+        while current and counter <= position:
+            if counter == position:
+                return current
+            current = current.next
+            counter += 1
+        return None
 
     def insert(self, new_element, position):
         """Insert a new node at the given position.
         Assume the first position is "1".
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
-        i = 1
+        counter = 1
         current = self.head
-        if position == 1:
-            self.head = new_element
-            new_element.next = current
-
-        if current:
-            while current and i <= position-1:
-                if i < position-1:
-                    current = current.next
-                    i += 1
-                elif i == position-1:
-                    temp = current.next
+        if position > 1:
+            while current and counter < position:
+                if counter == position - 1:
+                    new_element.next = current.next
                     current.next = new_element
-                    new_element.next = temp
-                    i += 1
-                # else:
-                #     print("position is out of list")
-        pass
+                current = current.next
+                counter += 1
+        elif position == 1:
+            new_element.next = self.head
+            self.head = new_element
 
     def delete(self, value):
         """Delete the first node with a given value."""
         current = self.head
-        if current:
-            if current.next.value != value:
-                current = current.next
+        previous = None
+        while current.value != value and current.next:
+            previous = current
+            current = current.next
+        if current.value == value:
+            if previous:
+                previous.next = current.next
+            else:
+                self.head = current.next
 
-            # elif current.value == value:
-            #     current =
-            #     temp = current.next.next
-            #
-            #     current.next = new_element
-            #     new_element.next = temp
-            #     i += 1
-        pass
+# Test cases
+# Set up some Elements
+e1 = Element(1)
+e2 = Element(2)
+e3 = Element(3)
+e4 = Element(4)
 
+# Start setting up a LinkedList
+ll = LinkedList(e1)
+ll.append(e2)
+ll.append(e3)
 
-ll = LinkedList(head=Element(2))
-ll.append(Element(2))
-ll.append(Element(7))
-ll.append(Element(5))
-ll.append(Element(7))
-ll.print()
-pos = ll.get_position(5)
-ll.insert(new_element=Element(6), position=9)
-print("add 6 to position 2")
-ll.print()
-print("DAMN!")
+# Test get_position
+# Should print 3
+print(ll.head.next.next.value)
+# Should also print 3
+print(ll.get_position(3).value)
+
+# Test insert
+ll.insert(e4, 3)
+# Should print 4 now
+print(ll.get_position(3).value)
+
+# Test delete
+ll.delete(1)
+# Should print 2 now
+print(ll.get_position(1).value)
+# Should print 4 now
+print(ll.get_position(2).value)
+# Should print 3 now
+print(ll.get_position(3).value)
