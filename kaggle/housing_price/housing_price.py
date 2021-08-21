6d673b5a3d0cb936bad50da0041624be94600297
ron.l <ron.leyzer@sparks-ab.com>
2021-08-21 12:26:14 +0300

    wip

--- kaggle/housing_price/housing_price.py
+++ kaggle/housing_price/housing_price.py
@@ -82,7 +82,6 @@ y = df['SalePrice']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
 
-
 ''' *** feature selection *** '''
 
 
