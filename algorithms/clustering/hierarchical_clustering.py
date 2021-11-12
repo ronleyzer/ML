229480ac4cefcb33e78f3940c237b6f3536421a2
ron.l <ron.leyzer@sparks-ab.com>
2021-11-12 17:58:03 +0200

    hierarchical_clustering

---
+++ algorithms/clustering/hierarchical_clustering.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.preprocessing import normalize
+import scipy.cluster.hierarchy as shc
+
+'''
+•	This algorithm starts with all the data points assigned to a cluster of their own. 
+•	Then two nearest clusters are merged into the same cluster.
+•	In the end, this algorithm terminates when there is only a single cluster left.
+https://www.analyticsvidhya.com/blog/2019/05/beginners-guide-hierarchical-clustering/
+'''
+
+
+def main():
+    # get the data
+    data = pd.read_csv(r'P:\ML\data\Wholesale customers data.csv')
+    # normalize
+    data_scaled = normalize(data)
+    data_scaled = pd.DataFrame(data_scaled, columns=data.columns)
+    # visualize the raw data
+    plt.figure(figsize=(10, 7))
+    plt.scatter(data_scaled['Milk'], data_scaled['Grocery'])
+    plt.show()
+    # draw the dendrogram to decide the number of clusters
+    plt.figure(figsize=(10, 7))
+    plt.title("Dendrograms")
+    dend = shc.dendrogram(shc.linkage(data_scaled, method='ward'))
+    plt.show()
+    '''The vertical line with maximum distance is the blue line and hence we can decide two clusters'''
+    # apply hierarchical clustering for 2 clusters
+    cluster = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='ward')
+    cluster.fit_predict(data_scaled)
+    # visualize the two clusters
+    plt.figure(figsize=(10, 7))
+    plt.scatter(data_scaled['Milk'], data_scaled['Grocery'], c=cluster.labels_)
+    plt.show()
+    '''The advantage of not having to pre-define the number of clusters gives it quite an edge over k-Means.'''
+
+
+if __name__ == '__main__':
+    main()
