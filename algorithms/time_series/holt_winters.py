65d358ee1ddafd5546bfd6b184f57b2806fd0ce0
ron.l <ron.leyzer@sparks-ab.com>
2021-12-09 10:32:53 +0200

    wip

--- algorithms/time_series/holt_winters.py
+++ algorithms/time_series/holt_winters.py
@@ -7,10 +7,22 @@ from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
 
 def background():
-    print('''Holt-Winters Exponential Smoothening (HWES) works on the idea of smoothening the values of a 
-             Univariate Time Series Analysis to use them for forecasting future values. 
-             The idea is to assign exponentially decreasing weights giving more importance to more recent incidents. 
-             So, when we move back in time, we would see diminishing weights.''')
+    print('''Holt-Winters Exponential Smoothening (HWES) works on the idea of smoothening the values of a\n
+             Univariate Time Series Analysis to use them for forecasting future values.\n
+             The idea is to assign exponentially decreasing weights giving more importance to more recent incidents.\n
+             So, when we move back in time, we would see diminishing weights.\n\n
+             
+             Levels-\n
+             Looks like stairs.
+             
+             Trends-\n
+             If Levels change in a particular pattern, you can say that the Time Series follows a Trend.\n
+             Trends can be linear, square, logarithmic, exponential.\n
+             Trend is a vector, as it has both magnitude and direction.\n\n
+             
+             Seasonality-\n
+             Certain patterns that periodically repeated.
+             ''')
 
 
 def main():
@@ -28,14 +40,34 @@ def main():
 
     '''plotting the original data'''
     airline[['Thousands of Passengers']].plot(title='Passengers Data')
+    plt.show()
 
     '''decomposing the Time Series
     Look for: Levels, Trends and Seasonality in the data'''
+    # drop nan
+    missing_values_count = airline.isnull().sum(0).sort_values(ascending=False)
+    airline.fillna(method='ffill')
+    # decompose
     decompose_result = seasonal_decompose(airline['Thousands of Passengers'], model='multiplicative')
     decompose_result.plot()
+    plt.show()
     '''We can quite clearly see that the data has all 3, Levels, Trends, Seasonality.'''
 
-    '''Fitting the Data with Holt-Winters Exponential Smoothing'''
+    '''Define the weight coefficient Alpha and the Time Period, 
+    and set the DateTime frequency to a monthly level.'''
+    airline.index.freq = 'MS'
+    # m = Time Period
+    m = 12
+    alpha = 1 / (2 * m)
+
+    '''Fitting the Data with Holt-Winters Exponential Smoothing-
+    Fit this data on Single, Double, and Triple Exponential Smoothing respectively.
+    While Single HWES can take care of time series data that has levels, 
+    Double HWES can as well consider data with trends and Triple HWES can even handle Seasonality.'''
+
+    '''Single Exponential Smoothing'''
+    airline['HWES1'] = SimpleExpSmoothing(airline['Thousands of Passengers']).fit(smoothing_level=alpha, optimized=False, use_brute=True).fittedvalues
+    airline[['Thousands of Passengers', 'HWES1']].plot(title='Holt Winters Single Exponential Smoothing')
 
     print("HY")
 
